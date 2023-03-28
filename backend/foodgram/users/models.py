17a5dc0f18b2912999053339cb26fc4d1119801f
Eugene Miskiv <evgenymiskiv@gmail.com>
2023-03-28 12:45:46 +0300

    add custom user model

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='email',
+        unique=True
+    )
+
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$')]
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='имя',
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='фамилия',
+        blank=True
+    )
