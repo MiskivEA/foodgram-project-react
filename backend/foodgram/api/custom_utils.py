3b9d560b84775f332629db05f8c5cf204d289660
Eugene Miskiv <evgenymiskiv@gmail.com>
2023-04-15 21:37:38 +0300

    fix tag filter

--- backend/foodgram/api/custom_utils.py
+++ backend/foodgram/api/custom_utils.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters
 
+from app.models import Cart
+
 User = get_user_model()
 
 
@@ -12,14 +14,20 @@ class CustomPaginationClass(PageNumberPagination):
 class FavoriteFilter(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
-        # request.user = User.objects.get(pk=1) it`s for test
         is_favorited = request.query_params.get('is_favorited')
-        tags = request.query_params.getlist('tag')
-        print(is_favorited)
-        print(tags)
+        is_in_shopping_cart = request.query_params.get('is_in_shopping_cart')
+        tags = request.query_params.getlist('tags')
+
+        if is_in_shopping_cart is not None:
+            new_q_carts = request.user.carts.all()
+            queryset = [item.recipe for item in new_q_carts]
+
         if is_favorited is not None:
             new_q_favorite = request.user.favorite_recipes.all()
             queryset = [item.recipe for item in new_q_favorite]
+
         if tags is not None:
-            queryset = queryset.filter(tag__slug__in=tags)
+            if len(tags) > 0:
+                queryset = queryset.filter(tag__slug__in=tags)
+        print(queryset)
         return queryset
